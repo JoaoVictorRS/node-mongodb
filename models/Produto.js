bc24d81879f1ec36cccfadfc23a30dc7b905c2c1
JoaoVictorRS <joaososteam@gmail.com>
2024-04-05 21:46:51 -0300

    controllers alteradas

--- models/Produto.js
+++ models/Produto.js
@@ -1,11 +1,33 @@
 const mongoose = require('mongoose')
 
-const schema =  mongoose.Schema({
-    nome: String,
-    preco: Number,
-    tamanho: String,
-    tipo: String,
-    ingredientes: []
+const schema = mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+        minLength: [2, "Nome muito curto"],
+        maxLength: 50
+    },
+    preco: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: [0, "Valor negativo"],
+        match: [/^\d+(\.\d{1,2})?$/, 'Formato invalido']
+    },
+    tamanho: {
+        type: String,
+        required: false,
+        lowercase: true,
+        enum: {
+            values: ['grande', 'media', 'pequena'],
+            message: '{VALUE} Invalido / Opções: grande, media, pequena'
+        }
+    },
+    tipo: {
+        type: String,
+        enum: ['Bebidas', 'Pizzas', 'Massas', 'Sobremesa']
+    },
+    ingredientes: [String]
 })
 
 const Produto = mongoose.model('Produto', schema)
